10fee76e9619b96068f1c1d20b4d69b437f6f9c5
James Abley <james.abley@gmail.com>
2015-06-22 22:07:14 +0100

    Go solution for eighth exercise of Set 1
    Remember that the problem with ECB is that it is stateless and
    deterministic; the same 16 byte plaintext block will always produce the
    same 16 byte ciphertext.
    
    So we examine the cipher text for repeating 16 byte blocks, and return
    the line that does this the most.

--- golang/set1.go
+++ golang/set1.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"math"
 	"os"
+	"reflect"
 )
 
 func Hex2Base64(hexBytes string) (string, error) {
@@ -143,6 +144,68 @@ func Challenge7() (res string, err error) {
 	}
 
 	return string(decodeAES(rawCipher, []byte("YELLOW SUBMARINE"))), nil
+}
+
+func Challenge8() (res string, err error) {
+	file, err := os.Open("../inputs/8.txt")
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	// read each line, hex decode the bytes and test it to see if it's ECB-encrypted
+	bestScore := -1
+	decrypted := ""
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		hexBytes := scanner.Bytes()
+		cipher, err := decodeHex(hexBytes)
+
+		if err != nil {
+			return decrypted, err
+		}
+
+		score := scoreECB(cipher)
+
+		if score > bestScore {
+			bestScore = score
+			decrypted = string(hexBytes)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	return decrypted, nil
+}
+
+func decodeHex(hexBytes []byte) ([]byte, error) {
+	cipher := make([]byte, hex.DecodedLen(len(hexBytes)))
+	_, err := hex.Decode(cipher, hexBytes)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cipher, nil
+}
+
+func scoreECB(cipher []byte) int {
+	blockLength := len(cipher) / aes.BlockSize
+	score := 0
+
+	for i := 0; i < blockLength; i++ {
+		offset := i * aes.BlockSize
+		block := cipher[offset : offset+aes.BlockSize]
+		for j := i + 1; j < blockLength; j++ {
+			otherOffset := j * aes.BlockSize
+			otherBlock := cipher[otherOffset : otherOffset+aes.BlockSize]
+			if reflect.DeepEqual(block, otherBlock) {
+				score++
+			}
+		}
+	}
+
+	return score
 }
 
 func decodeAES(cipher []byte, key []byte) []byte {
