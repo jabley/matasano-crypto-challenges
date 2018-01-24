aa5e3e7fb33f2706cecd4d639c9306c060ccf416
James Abley <james.abley@gmail.com>
2018-01-24 19:53:56 +0000

    Go solution for Challenge 17, Set 3
    This is the best-known attack on modern block-cipher cryptography.
    
    Combine your padding code and your CBC code to write two functions.
    
    The first function should select at random one of the following 10 strings:
    
    * MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=
    * MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=
    * MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==
    * MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==
    * MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl
    * MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==
    * MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==
    * MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=
    * MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=
    * MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93
    
    generate a random AES key (which it should save for all future
    encryptions), pad the string out to the 16-byte AES block size and
    CBC-encrypt it under that key, providing the caller the ciphertext and
    IV.
    
    The second function should consume the ciphertext produced by the first
    function, decrypt it, check its padding, and return true or false
    depending on whether the padding is valid.
    
    What you're doing here
    ======================
    This pair of functions approximates AES-CBC encryption as its deployed
    serverside in web applications; the second function models the server's
    consumption of an encrypted session token, as if it was a cookie.
    
    It turns out that it's possible to decrypt the ciphertexts provided by
    the first function.
    
    The decryption here depends on a side-channel leak by the decryption
    function. The leak is the error message that the padding is valid or
    not.
    
    You can find 100 web pages on how this attack works, so I won't
    re-explain it. What I'll say is this:
    
    The fundamental insight behind this attack is that the byte 01h is valid
    padding, and occur in 1/256 trials of "randomized" plaintexts produced
    by decrypting a tampered ciphertext.
    
    02h in isolation is not valid padding.
    
    02h 02h is valid padding, but is much less likely to occur randomly than
    01h.
    
    03h 03h 03h is even less likely.
    
    So you can assume that if you corrupt a decryption AND it had valid
    padding, you know what that padding byte is.
    
    It is easy to get tripped up on the fact that CBC plaintexts are
    "padded". Padding oracles have nothing to do with the actual padding on
    a CBC plaintext. It's an attack that targets a specific bit of code that
    handles decryption. You can mount a padding oracle on any CBC block,
    whether it's padded or not.

---
+++ golang/set3.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+)
+
+func newCBCPaddingOracle(plainText []byte) (
+	encrypt func() []byte,
+	isValidPadding func([]byte) bool,
+) {
+	b, _ := aes.NewCipher(newKey())
+	iv := newIv()
+	blockCipher := newAESCBCBlockCipher(b, iv)
+
+	encrypt = func() []byte {
+		in := padPKCS7([]byte(plainText), 16)
+		res, err := blockCipher.encrypt(in)
+		if err != nil {
+			panic(err)
+		}
+		// See how the attack works, at the start of attackCBCPadding...
+		// We need to return the iv as the first block, and the cipher
+		// text as the following blocks
+		return append(iv, res...)
+	}
+
+	isValidPadding = func(in []byte) bool {
+		out, err := blockCipher.decrypt(in)
+		if err != nil {
+			panic(err)
+		}
+		return isPKCS7Padded(out, 16)
+	}
+
+	return
+}
+
+func attackCBCPadding(encrypted []byte, isValidPadding func([]byte) bool) []byte {
+	// From https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Cipher_Block_Chaining_(CBC)
+	// We only need 2 blocks:
+	// * the second block will contain the PKCS7 padding
+	// * the first block will act like the initialisation vector in CBC decryption
+
+	// for the 2nd block, we might have plain text of:
+	//
+	// 1. XXXXXXXXXXX55555
+	// 2. XXXXXXXXXXXXXXX1
+	// 3. XXXXXXXXXXXXXX21
+	// 4. XXXXXXXXXXXXXXXX
+	//
+	// If we twiddle the cipher text blocks and manage to set the last byte to
+	// 1 when it's been decrypted, then that will be valid PKCS7 padding.
+	//
+	// Applying this to our test cases above:
+	//
+	// 1. will seem to have valid padding when we've set the last byte to 1 or 5
+	// 2. will have valid padding when we've set the last byte to 1 or maybe 2 if the last plain text byte is 0x02)
+	// 3. will have valid padding when we've set the last byte to 1 or 2
+	// 4. will have valid padding when we've set the last byte to 1 (or maybe 2 if the last plain text byte is 0x02)
+	//
+	// So there are some edge cases that we'll need to detect as part of our implementation
+	findNextByte := func(known, iv, block []byte) []byte {
+		if len(block) != 16 || len(iv) != 16 || len(known) >= 16 {
+			panic("wrong lengths for findNextByte")
+		}
+
+		buf := make([]byte, 32)
+		copy(buf[16:], block)
+		plaintext := append([]byte{0x00}, known...)
+		padding := byte(len(plaintext))
+
+		for i := 0; i < 256; i++ {
+			copy(buf, iv)
+			plaintext[0] = byte(i)
+
+			for i := range plaintext {
+				// erase the known bytes by flipping them
+				buf[len(buf)-1-16-i] ^= plaintext[len(plaintext)-1-i]
+				// apply valid padding for the current byte we're attacking
+				buf[len(buf)-1-16-i] ^= padding
+			}
+
+			// check we actually changed something. If we've produced
+			// the same as the `iv`` after all our bit-twiddling, we
+			// already know that's valid because it's what our
+			// (hopefully correct!) encryption function produced.
+			if bytes.Equal(buf[:16], iv) {
+				continue
+			}
+
+			if isValidPadding(buf) {
+				return plaintext
+			}
+		}
+
+		// if the only one that works is not changing anything, there's
+		// already a padding of `padding`. This is the edge cases
+		// described in 2. and 4. above.
+		plaintext[0] = padding
+		for _, c := range plaintext {
+			if c != padding {
+				plaintext[1] ^= padding
+				return plaintext[1:] // correct and retry
+			}
+		}
+
+		return plaintext
+	}
+
+	if len(encrypted)%16 != 0 {
+		panic("unexpected cipher text length")
+	}
+
+	nBlocks := len(encrypted) / 16
+	var plainText []byte
+	for i := nBlocks; i > 1; i-- {
+		var known []byte
+		start := (i - 1) * 16
+		iv := encrypted[start-16 : start]
+		block := encrypted[start : start+16]
+
+		for len(known) < 16 {
+			known = findNextByte(known, iv, block)
+		}
+
+		plainText = append(known, plainText...)
+	}
+
+	return plainText
+}
--- golang/set3_test.go
+++ golang/set3_test.go
@@ -30,3 +30,27 @@ func TestUnpadPKCS7(t *testing.T) {
 		})
 	}
 }
+
+func TestChallenge17(t *testing.T) {
+	tests := []struct {
+		in       string
+		expected string
+	}{
+		{"MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=", "000000Now that the party is jumping"},
+		{"MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=", "000001With the bass kicked in and the Vega's are pumpin'"},
+		{"MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==", "000002Quick to the point, to the point, no faking"},
+		{"MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==", "000003Cooking MC's like a pound of bacon"},
+		{"MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl", "000004Burning 'em, if you ain't quick and nimble"},
+		{"MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==", "000005I go crazy when I hear a cymbal"},
+		{"MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==", "000006And a high hat with a souped up tempo"},
+		{"MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=", "000007I'm on a roll, it's time to go solo"},
+		{"MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=", "000008ollin' in my five point oh"},
+		{"MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93", "000009ith my rag-top down so my hair can blow"},
+	}
+
+	for _, test := range tests {
+		encryptMessage, isValidPadding := newCBCPaddingOracle(decodeBase64(t, test.in))
+		out := encryptMessage()
+		assertEqual(t, test.expected, string(unpadPKCS7(attackCBCPadding(out, isValidPadding))))
+	}
+}
